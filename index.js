f0c629c660ab195f60c2d417bad2ed56b5a6b663
NataliiaBoiko22 <nata.boyko.kras@gmail.com>
2023-05-14 11:33:52 +0200

    feat: add list action

---
+++ index.js
@@ -0,0 +1,26 @@
+const contacts = require("./contacts");
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+const argv = program.opts();
+program.parse(process.argv);
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      console.table(allContacts);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+invokeAction(argv);
